8e921c5dd180c0c0c975dcc717d5244f3f7260f3
Cesar Sepulveda <cesar.sepulveda@xapo.com>
2021-06-22 12:00:57 -0400

    variable domain name

--- static-website.tf
+++ static-website.tf
@@ -1,8 +1,8 @@
 module "cloudfront_s3_website_with_domain" {
-    source                 = "chgangaraju/cloudfront-s3-website/aws"
-    version                = "1.2.2"
-    hosted_zone            = "csepulveda.io" 
-    domain_name            = "blog.csepulveda.io"
-    acm_certificate_domain = "*.csepulveda.io"
-    upload_sample_file     = true
+  source                 = "chgangaraju/cloudfront-s3-website/aws"
+  version                = "1.2.2"
+  hosted_zone            = var.primary_domain_name
+  domain_name            = "blog.${var.primary_domain_name}"
+  acm_certificate_domain = "blog.${var.primary_domain_name}"
+  upload_sample_file     = true
 }
