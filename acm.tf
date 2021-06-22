c61a467f325be59f5c1879ea90f81b878758e8df
Cesar Sepulveda <cesar.sepulveda@xapo.com>
2021-06-22 12:10:58 -0400

    testing maniac module

--- acm.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_acm_certificate" "blog" {
-  domain_name       = "blog.${var.primary_domain_name}"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
--- static-website.tf
+++ static-website.tf
@@ -1,8 +1,6 @@
-module "cloudfront_s3_website_with_domain" {
-  source                 = "chgangaraju/cloudfront-s3-website/aws"
-  version                = "1.2.2"
-  hosted_zone            = var.primary_domain_name
-  domain_name            = "blog.${var.primary_domain_name}"
-  acm_certificate_domain = aws_acm_certificate.blog.domain_name
-  upload_sample_file     = true
+module "aws_static_website" {
+  source = "cloudmaniac/static-website/aws"
+
+  website-domain-main     = "csepulveda.io"
+  website-domain-redirect = "blog.csepulveda.io"
 }
