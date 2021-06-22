fc87ad8cea44914a2fe36f587c921dec121f0a4f
Cesar Sepulveda <cesar.sepulveda@xapo.com>
2021-06-22 12:06:15 -0400

    added acm

--- static-website.tf
+++ static-website.tf
@@ -3,6 +3,6 @@ module "cloudfront_s3_website_with_domain" {
   version                = "1.2.2"
   hosted_zone            = var.primary_domain_name
   domain_name            = "blog.${var.primary_domain_name}"
-  acm_certificate_domain = "blog.${var.primary_domain_name}"
+  acm_certificate_domain = aws_acm_certificate.blog.domain_name
   upload_sample_file     = true
 }
